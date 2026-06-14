3e5be3cdbe2d97444ecc35f046eb1e443bcd0a4c
galopemach@gmail.com <galopemach@gmail.com>
2026-06-15 00:44:45 +0800

    Fixed

--- Playwright/tests/login.spec.js
+++
@@ -1,7 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('Youtube title test', async ({ page }) => {
-    await page.goto('https://www.youtube.com');
-
-    await expect(page).toHaveTitle(/YouTube/);
-});
