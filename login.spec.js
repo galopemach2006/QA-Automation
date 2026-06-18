4146154f5e502d3b20b4f517f0eb5dc10795da9f
galopemach@gmail.com <galopemach@gmail.com>
2026-06-18 23:55:40 +0800

    In Progress

---
+++ login.spec.js
@@ -0,0 +1,13 @@
+import {test, expect} from "@playwright/test"
+
+test("Login Test: Username - admin, Password - admin123", async ({page}) => {
+    await page.goto("https://practice.qabrains.com/")
+    await page.getByPlaceholder("eg. user@user.com").fill("admin")
+    await page.getByPlaceholder('*******').fill("admin123")
+    await page.getByRole('button', {name: "Login"}).click()
+    await page.waitForTimeout(5000)
+})
+
+test("Login Test ", async ({page}) => {
+    await page.goto("https://practice.qabrains.com/")
+})
