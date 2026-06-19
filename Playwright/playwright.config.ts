668e8e42480365a16742071c4a527607cc3d644a
galopemach@gmail.com <galopemach@gmail.com>
2026-06-19 14:50:23 +0800

    Changed from javascript to typescript

--- Playwright/playwright.config.ts
+++ Playwright/playwright.config.ts
@@ -1,4 +1,3 @@
-// @ts-nocheck
 import { defineConfig, devices } from '@playwright/test';
 
 /**
@@ -10,10 +9,10 @@ import { defineConfig, devices } from '@playwright/test';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
- * @see https://playwright.dev/docs/test-configuration
+ * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './Tests',
+  testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -21,11 +20,9 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers:  1 ,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ['html', {open: "never"}]
-  ],
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -39,16 +36,10 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { 
-        ...devices['Desktop Chrome'],
-        viewport : {width : 1920, height : 1080},
-        video : 'on',
-        screenshot : 'only-on-failure',
-        trace: 'on',
-        headless : true,
-      },
+      use: { ...devices['Desktop Chrome'] },
     },
-  /*
+
+    /*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -58,8 +49,6 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-  */
-
 
     /* Test against mobile viewports. */
     // {
@@ -89,4 +78,3 @@ export default defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
