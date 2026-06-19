77aed0d8cd4c88cb2414e77e8f52e409815cf3a2
galopemach@gmail.com <galopemach@gmail.com>
2026-06-19 18:53:32 +0800

    changed line 1

--- Playwright/tests/Website_Practice_2/login.spec.ts
+++ Playwright/tests/Website_Practice_2/login.spec.ts
@@ -1,5 +1,6 @@
 import {test, expect} from "@playwright/test"
 
+//Should show invalid credentials
 test("Login Test: Username - admin, Password - admin123", async ({page}) => {
     await page.goto("https://practice.qabrains.com/")
     await page.getByPlaceholder("eg. user@user.com").fill("admin")
@@ -8,6 +9,9 @@ test("Login Test: Username - admin, Password - admin123", async ({page}) => {
     await page.waitForTimeout(5000)
 })
 
+//Should require a password
 test("Login Test: Username - admin, Password - Blank Field", async ({page}) => {
     await page.goto("https://practice.qabrains.com/")
+    await page.getByPlaceholder("eg. user@user.com").fill("admin@1234")
+    await page.getByRole('button', {name: "Login"}).click()
 })
